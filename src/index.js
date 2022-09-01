c2f747993018e623937b6bb8b5e2f8074d439a20
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2022-09-01 18:55:57 +0600

    modified:   src/index.css 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 
-
-
 class Square extends React.Component {
+  
+
+
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square />;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={()=> this.handClick[i]}  
+    />;
   }
 
   render() {
